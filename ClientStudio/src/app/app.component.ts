04e00dd71ffc84ba8edb827453dbd8b4e38472d5
keshav-nandhan <keshavthehellrider@gmail.com>
2018-06-20 22:12:42 +0530

    Initial commit

---
+++ ClientStudio/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles:['./styles.css'],
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Image Gallery';
+}
